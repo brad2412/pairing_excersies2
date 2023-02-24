423b03520ee699518141c04150a0b64a99e11705
Bradley Milton <bmilton2412@gmail.com>
2023-02-23 20:22:42 -0800

    add an additional condition to our if statement

--- plants.rb
+++ plants.rb
@@ -1,9 +1,11 @@
-gets_sunshine = false
-ounces_of_water = 10
+gets_sunshine = true
+ounces_of_water = 9
     if gets_sunshine == true && ounces_of_water >= 10
         puts "The plant will thrive!"
-    elsif gets_sunshine == false || ounces_of_water < 10
+    elsif gets_sunshine == false && ounces_of_water < 10
         puts "The plant will DIE!"
+    elsif gets_sunshine == false || ounces_of_water < 10
+        puts "The plant either needs sunshine or water."
     else 
         puts "The plant will DIE!"
     end
