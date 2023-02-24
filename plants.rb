fd87275634947210643f28a8cac9f2d55f481362
Bradley Milton <bmilton2412@gmail.com>
2023-02-23 20:14:35 -0800

    add plants.rb conditional statement

---
+++ plants.rb
@@ -0,0 +1,11 @@
+gets_sunshine = false
+ounces_of_water = 10
+    if gets_sunshine == true && ounces_of_water >= 10
+        puts "The plant will thrive!"
+    elsif gets_sunshine == false || ounces_of_water < 10
+        puts "The plant will DIE!"
+    else 
+        puts "The plant will DIE!"
+    end
+
+    
